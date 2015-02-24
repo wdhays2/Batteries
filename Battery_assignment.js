6927b9fc2319895eec78092603159c82c3ec261e
wdhays2 <wdhays2@charter.net>
2015-02-24 09:57:54 -0800

    Add formulas.

--- Battery_assignment.js
+++ Battery_assignment.js
@@ -1,3 +1,21 @@
 var battery = {
   voltage: 4, mAh: 2000, priceEach: 2
-  }
+}
+
+var userSeriesInput = prompt();
+
+var seriesVolts = this.voltage * userSeriesInput;
+
+var seriesAmps = this.mAh;
+
+var seriesPrice = this.priceEach * userSeriesInput;
+
+
+var userParallelInput = prompt();
+
+var parallelVolts = this.voltage; 
+
+var parallelAmps = this.mAh * userParallelInput;
+
+var parallelPrice = this.priceEach * userParallelInput;
+
