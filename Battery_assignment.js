fb3e4cfbeabd331c29217b632c6c7592b481fea1
weshays <weshays@gbdev.com>
2015-03-04 14:26:28 -0800

    updated Battery assigment and how the building of the cell works.

--- Battery_assignment.js
+++ Battery_assignment.js
@@ -1,26 +1,37 @@
-function buildCell(type, volts, maxmAh, mAhleft, priceEach, rechar){
-  function cell (type, volts, maxmAh, mAhleft, priceEach, rechar){    
-    type: function() {return type;},
-    volts: volts,
-    maxmAh: function() {return maxmAh;},
-    mAhleft: mAhleft,
-    priceEach: function() {return priceEach;},
-    rechar: function() {return rechar;},
+function BuildCell(type, volts, maxmAh, mAhleft, priceEach, rechar){
+  function cell() {   
+    this.type = function() {return type;};
+    this.volts = volts;
+    this.maxmAh = function() {return maxmAh;}
+    this.mAhleft = mAhleft;
+    this.priceEach = function() {return priceEach;};
+    this.rechar = function() {return rechar;};
   }
-  return ;
+  return cell;
 }
-var Panasonic18650 = buildCell("Li-ion", 3.7, 3400, 0, 7.50, true);
-var China18650 = buildCell("Li-ion", 4.0, 2000, 0, 2.00, true);
-var TenergyD = buildCell("NiMH", 1.2, 10000, 0, 7.36, true);
-var DuracellD = buildCell("Alkaline", 1.5, 14000, 14000, 2.52, false);
-var DuracellC = buildCell("Alkaline", 1.5, 7000, 7000, 1.42, false);
-var DuracellAA = buildCell("Alkaline", 1.5, 2100, 2100, 0.50, false);
-var DuracellAAA = buildCell("Alkaline", 1.5, 1000, 1000, 0.52, false);
-var EneloopAA = buildCell("NiMH", 1.2, 2000, 2000, 3.09, true);
-var EneloopAAA = buildCell("NiMH", 1.2, 750, 750, 2.25, true);
+var Panasonic18650 = BuildCell("Li-ion", 3.7, 3400, 0, 7.50, true);
+var China18650 = BuildCell("Li-ion", 4.0, 2000, 0, 2.00, true);
+var TenergyD = BuildCell("NiMH", 1.2, 10000, 0, 7.36, true);
+var DuracellD = BuildCell("Alkaline", 1.5, 14000, 14000, 2.52, false);
+var DuracellC = BuildCell("Alkaline", 1.5, 7000, 7000, 1.42, false);
+var DuracellAA = BuildCell("Alkaline", 1.5, 2100, 2100, 0.50, false);
+var DuracellAAA = BuildCell("Alkaline", 1.5, 1000, 1000, 0.52, false);
+var EneloopAA = BuildCell("NiMH", 1.2, 2000, 2000, 3.09, true);
+var EneloopAAA = BuildCell("NiMH", 1.2, 750, 750, 2.25, true);
+
+
+var bat1 = new Panasonic18650();
+var bat2 = new Panasonic18650();
+
+console.log( bat1.type() );
+console.log( bat1.volts );
+
+
+
 
 var currentDraw = 0;
 var hoursUsed = 0;
+
 
 Battery.prototype = {
   charge: function(){
