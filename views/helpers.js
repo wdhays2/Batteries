993c21a05a7b4935ffdb5c8ded4b8dfe6b4677c0
wdhays2 <wdhays2@charter.net>
2015-03-11 08:01:27 -0700

    add display functions, start work on suggest functions

--- views/helpers.js
+++ views/helpers.js
@@ -24,19 +24,30 @@ $( document ).ready(function() {
     var numParallel = parseInt( $("#userInputPar").val() );
     batteryHolder = buildBatt(batCell, numSeries, numParallel);
     updateBatteryView();
+    displayVolts();
+    displayAmps();
+    displayPrice();
+    suggestVolts();    
   });
+  
 });
 
 
 // Global Variables
 // ---------------------------
 var batteryHolder;
+var numSeriesSuggest;
+var numParallelSuggest;
+var wantVolts = 0;
+var wantAmps = 0;
+var wantPrice = 0;
 // ---------------------------
 
 
 function selectCell(){
   var batCell = "";
   batCell = $("#cellOptions").val();
+  batteryHolder = buildBatt(batCell, 1, 1);
   console.log(batCell);
 }
 
@@ -52,20 +63,17 @@ function selectPar(){
   console.log(numParallel);
 }
 
-function calcVolts(){
-  var wantVolts = 0;
+function calcVolts(){  
   wantVolts = $("#userVolts").val();
   console.log(wantVolts);
 }
 
-function calcAmps(){
-  var wantAmps = 0;
+function calcAmps(){  
   wantAmps = $("#userAmps").val();
   console.log(wantAmps);
 }
 
-function calcPrice(){
-  var wantPrice = 0;
+function calcPrice(){  
   wantPrice = $("#userPrice").val();
   console.log(wantPrice);
 }
@@ -73,36 +81,62 @@ function calcPrice(){
 function stringToCellType(cellStr) {
   var batCell = 'NOT SET';
   switch(cellStr) {
-    case 'Pan18650':
-      batCell = Pan18650;
-      break;
-    case 'Gen18650':
-      batCell = Gen18650;
-      break;
-    case 'TgyD':
-      batCell = TgyD;
-      break;
-    case 'DclD':
-      batCell = DclD;
-      break;
-    case 'DclC':
-      batCell = DclC;
-      break;
-    case 'DclAA':
-      batCell = DclAA;
-      break;
-    case 'DclAAA':
-      batCell = DclAAA;
-      break;
-    case 'EnlAA':
-      batCell = EnlAA;
-      break;
-    case 'EnlAAA':
-      batCell = EnlAAA;
-      break;
+    case 'Pan18650': batCell = Pan18650; break;
+    case 'Gen18650': batCell = Gen18650; break;
+    case 'TgyD': batCell = TgyD; break;
+    case 'DclD': batCell = DclD; break;
+    case 'DclC': batCell = DclC; break;
+    case 'DclAA': batCell = DclAA; break;
+    case 'DclAAA': batCell = DclAAA; break;
+    case 'ElpAA': batCell = ElpAA; break;
+    case 'ElpAAA': batCell = ElpAAA; break;
   } 
   return batCell;
 }
+
+function displayVolts(){
+  var custVolts = checkVolts(batteryHolder);
+  $("#actualVolts").html(custVolts);
+  return custVolts;
+}
+
+function displayAmps(){
+  var custAmps = checkAmps(batteryHolder);
+  $("#actualAmps").html(custAmps);
+  return custAmps;
+}
+
+function displayPrice(){
+  var custPrice = totalPrice(batteryHolder);
+  $("#totalPrice").html(custPrice);
+  return custPrice;
+}
+
+function suggestVolts(){
+  var batCell = stringToCellType( $("#cellOptions").val() );  
+  batteryHolder = buildBatt(batCell, 1, 1);
+  var userVolts = calcVolts();
+  temp = matchUserVolts(batCell, userVolts);
+  numSeriesSuggest = parseInt(temp);  
+  console.log(numSeriesSuggest);
+}
+
+function suggestAmps(){
+  var batCell = stringToCellType( $("#cellOptions").val() );
+  var userAmps = parseInt( $("#userAmps").val() );
+  temp = matchUserAmps(batCell, userAmps);
+  numParallelSuggest = parseInt(temp);
+  $("#userInputPar").html(numParallelSuggest);
+  return numParallelSuggest;
+}
+
+
+
+
+
+
+
+
 
 function updateBatteryView() {
   // [
@@ -115,7 +149,7 @@ function updateBatteryView() {
     for(var j=0; j<batteryHolder[i].length; j++) { // series
       batStr += ' == ';
     }
-    batStr += '] ';
+    batStr += '] <br>';
   }
   batStr += ' ] ';
 
